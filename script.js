b8321deb9fb7e82a660f9f239963196ab3bce848
wiusxt <ashatovanas65@gmail.com>
2022-02-28 14:41:08 +0600

    added new task with foreach

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+// let fruits = ["apple", "banana", "qiwi", "peach"]
+// fruits.forEach(element => {
+//     console.log(element);
+// });
+
+let fruits = ["apple",
+"banana",
+false,
+13,
+"hello",
+["1", "2", "3"],
+{name: "Peter", Lastname: "parker"},
+12345
+
+]
+ 
+
