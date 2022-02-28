8bb53a29b8c8ec1700977c40d7b5d1edd12be9f0
wiusxt <ashatovanas65@gmail.com>
2022-02-28 14:57:37 +0600

    added new task

--- script.js
+++ script.js
@@ -5,15 +5,38 @@
 //     console.log(element);
 // });
 
-// let fruits = ["apple",
-// "banana",
-// false,
-// 13,
-// "hello",
-// ["1", "2", "3"],
-// {name: "Peter", Lastname: "parker"},
-// 12345
+let fruits = ["apple",
+"banana",
+false,
+13,
+"hello",
+["1", "2", "3"],
+{name: "Peter", Lastname: "parker"},
+12345]
 
-// ]
- 
+
+// string
+// boolean
+// integer
+// string
+// array
+// object
+// integer
+
+fruits.forEach(item => {
+    console.log((typeof Boolean))
+})
+
+let fruits = {
+   name: "Peter",
+   Lastname: "Parker" ,
+   age: 34
+},
+{
+    name: Lily,
+    Lastname: RoseDepp,
+}
+   
+
+
 
