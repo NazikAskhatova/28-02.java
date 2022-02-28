da7faa2ce284b321c98c29176214ca32bee5f65e
wiusxt <ashatovanas65@gmail.com>
2022-02-28 14:41:40 +0600

    added massive

--- script.js
+++ script.js
@@ -5,15 +5,15 @@
 //     console.log(element);
 // });
 
-let fruits = ["apple",
-"banana",
-false,
-13,
-"hello",
-["1", "2", "3"],
-{name: "Peter", Lastname: "parker"},
-12345
+// let fruits = ["apple",
+// "banana",
+// false,
+// 13,
+// "hello",
+// ["1", "2", "3"],
+// {name: "Peter", Lastname: "parker"},
+// 12345
 
-]
+// ]
  
 
